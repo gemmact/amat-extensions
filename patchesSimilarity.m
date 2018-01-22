c2fe3a8d918ac002b6ab4805bb6bfbd0f8d00757
gemmact <gemmact95@gmail.com>
2018-01-22 15:48:44 -0500

    Name changes + heatmap in progress

--- patchesSimilarity.m
+++ patchesSimilarity.m
@@ -1,254 +1,291 @@
+function fh = patchesSimilarity(imgRGB)
+% DRAWDISKONFIGUREINTERACTIVE Draw reconstructed disk patches on the 
+%   original image and display useful information. This tool allows the 
+%   user to interactively change various parameters, such as the size of
+%   the disk, the type of the error function used, and the method used for
+%   summarizing an image patch. The supported functions include:
+% 
+%   [hover mouse over figure]: change the coordinates of the disk center.
+%   [left click]: change the error type used ({'se'},'mse','nmse','rse',
+%                 'rmse','nrmse','dssim').
+%   [right click]:  change the encoding method ({'average'},'hist').
+%   [middle click]: change the number of bins used for the histogram
+%                   computations.
+%   [scroll wheel]: up/down (decrease/increase) the radius of the disk.
+%
+%   See also: patchEncoding
+% 
+%   Stavros Tsogkas <tsogkas@cs.toronto.edu>
+%   Last update: November 2016
 
-function h = patchesSimilarity(im)
-global B imRGB
+% Default parameters
+r = 15;
+numBins = 32; % used for histogram encodings
+methods.error    = {'se','mse','nmse','rse','rmse','nrmse','dssim'};
+methods.encoding = {'average','hist-smirnov','hist-expectation','hist-mode'};
+encodingType  = 'hist-normalized';
+errorType = 'se';
+errorCounter = find(strcmp(methods.error, errorType));
+encodingCounter = find(strcmp(methods.encoding, encodingType));
 
-imRGB = imread(im);
-
-patchSize = 32;
-B = 32;
-
-global x1ref y1ref
-
-
-imLab = rgb2labNormalized(imRGB);
-
-x1ref = -32;
-y1ref = -32;
-
-
-
-[H,W,C] = size(imRGB);
-RI = imref2d(size(imRGB));
-RI.XWorldLimits = [1 W];
-RI.YWorldLimits = [1 H];
-fh = figure(1); 
-subplot(3,6,[1 15])
-imshow(imRGB, RI, 'InitialMagnification', 'fit');
-set(fh, 'WindowButtonDownFcn', @firstPoint);
+% Plot figure and set callbacks
+fh = figure(1);
+subplot(3,6,[1 15]);
+imshow(imgRGB);
+im = imgRGB;
 set(fh, 'WindowButtonMotionFcn', @changePoint);
+set(fh, 'WindowButtonDownFcn',   @changeReferencePatch);
+set(fh, 'WindowScrollWheelFcn',  @changeRadius);
+[H,W,C] = size(imgRGB);
+imgGray = rgb2gray(imgRGB); 
+imgTexture = textonMap(imgGray, numBins);
+size(imgTexture)
+%imgTexture    = reshape(imgTexture,H*W,[]);
+%tmap    = textonMap(imgRGB, numBins); 
+%tmap    = reshape(tmap,H*W,[]);
+imgRGB  = reshape(imgRGB, [], C);
+imgLab  = rgb2labNormalized(imgRGB);
+refPatch= zeros(2*r+1);
+refxy   = [];
+encPatch = zeros((C+1)*(1 + r + 1 + (r + 1)/4), numBins);
+encRefPatch = zeros((C+1)*(1 + r + 1 + (r + 1)/4), numBins);
+isReferenceFrameSelected = false;
+selectingNewReference = false;
+[xx,yy] = meshgrid(1:W,1:H);
+w1 = 0.05*ones(1,4);
+w2 = 0.01875*ones(1,16);
+w3 = 0.0078125*ones(1,64);
+showHomogeneityHeatmap()
 
-global ii1 ii2 ii3 ii4
 
-ii1 = prepareHistogram(imLab(:, :, 1), B);
-ii2 = prepareHistogram(imLab(:, :, 2), B);
-ii3 = prepareHistogram(imLab(:, :, 3), B);
-
-img = rgb2gray(im2double(imRGB)); 
-texture = textonMap(img, B);
-ii4 = prepareHistogram(texture, B);
-
-
-
-end
-
-
-
-
-function firstPoint(fh,~)
-        global h1 B ii1 ii2 ii3 ii4 x1ref y1ref imRGB
-        %im = imread(im);
-        im = imRGB;
-        [H,W,C] = size(im);
-        
+    function drawSquare(fh)
+        % Get point coordinates and check for validity
+        fh = figure(1);
+        subplot(3,6,[1 15]);
+        imshow(im);
         x = round(fh.CurrentAxes.CurrentPoint(1,1));
         y = round(fh.CurrentAxes.CurrentPoint(1,2));
+        if x < 1 || x > W || y < 1 || y > H
+            title('You are outside of the figure'); drawnow; return
+        end
+        if x-r < 1 || x+r > W || y-r < 1 || y+r > H
+            title('Square crosses the image boundary'); drawnow; return
+        end
         
-        RI = imref2d(size(im));
-        RI.XWorldLimits = [1 W];
-        RI.YWorldLimits = [1 H];
-         
-        
-        subplot(3,6,[1 15])
-        imshow(im, RI, 'InitialMagnification', 'fit');
-        
-        
-        
-        P = 32; %patchsize
-        %x = 50;
-        %y = 50;
-        x1 = x - floor(P/2);
-        x2 = x + ceil(P/2) - 1;
-        y1 = y - floor(P/2);
-        y2 = y + ceil(P/2) - 1;
-        
-        x1ref = x1;
-        y1ref = y1;
-       
-        
-        if (x1>= 1 && x2<= W && y1>=1 && y2<=H)
-            
-            rectangle('Position', [x1, y1, P, P], 'EdgeColor', 'r');
+        % Square logical mask
+        D = abs(xx-x) <= r & abs(yy-y) <= r;
 
-            h1 = zeros(84, B);
-            %h1 = zeros(4,B);
-            l = 1;
-            for k = 2.^(0:2)
-                patchSize = P/k;
-                for i = x1:patchSize:x2-patchSize+1
-                    for j = y1:patchSize:y2-patchSize+1
-                        
-                        h1(4*l-3, :) = createHistogram(ii1, i, j, patchSize);
-                        h1(4*l-2, :) = createHistogram(ii2, i, j, patchSize);
-                        h1(4*l-1, :) = createHistogram(ii3, i, j, patchSize);
-                        h1(4*l, :) = createHistogram(ii4, i, j, patchSize);
-                        l = l + 1;
-                        
-                        
-                    end
+        % The dssim metric should be used on RGB data
+        if strcmp(errorType, 'dssim')
+            imgPatch = imgRGB(D,:);
+        else
+            imgPatch = imgLab(D,:);
+        end
+        
+        if isReferenceFrameSelected
+            figure(1)
+            % Keep plotting the reference frame
+            rectangle('Position', [refxy(1)-r, refxy(2)-r, 2*r, 2*r], 'EdgeColor', 'g');
+            
+            % Encode patch and subpatches
+            encPatch = subpatchesEncode(x, y);
+            
+
+            % Disable annoying docking error that clutters the command line
+            if strcmp(fh.WindowStyle, 'docked')
+                warning('off','images:imshow:magnificationMustBeFitForDockedFigure')
+            end
+            
+            
+            % Display image and compare/save patches
+            if selectingNewReference
+                
+                %save reference patch
+                refPatch = reshape(imgPatch, [2*r + 1, 2*r + 1, 3]);  
+                
+                % Encode patch and subpatches
+                encRefPatch = subpatchesEncode(x, y);
+                
+                %Create heatmap of similarity values between the reference
+                %patch and the other ones
+                
+                showSimilarityHeatmap(encRefPatch);
+                
+                %plot patch of reference and subpatches to study
+                figure(1)
+                
+                subplot(3,6,4);
+                imshow(refPatch);
+                
+                subplot(3,6,10);
+                displaySubpatches(refPatch, r + 1);
+                
+                subplot(3,6,16);
+                displaySubpatches(refPatch, floor(r/2) + 1);
+                
+            else
+                %save patch to compare with reference one
+                comparePatch = reshape(imgPatch, [2*r + 1, 2*r + 1, 3]);
+ 
+                %plot square we are comparing to reference one
+                rectangle('Position', [x-r, y-r, 2*r, 2*r], 'EdgeColor', 'k');
+                
+                %plot patch and subpatches to study
+                subplot(3,6,6)
+                imshow(comparePatch);
+                    
+                subplot(3,6,12)
+                displaySubpatches(comparePatch, r + 1);
+
+                subplot(3,6,18)
+                displaySubpatches(comparePatch, floor(r/2) + 1);
+                
+                %compare patches
+                dist = histogramDistance(encRefPatch, encPatch, 'chi2');
+                
+                
+                %plot similarity scores between histograms
+                
+                subplot(3,6,5, 'replace')
+                displayScores(0, 1, dist);
+
+                subplot(3,6,11, 'replace')
+                displayScores(1, 5, dist);
+
+                subplot(3,6,17, 'replace')
+                displayScores(3, 21, dist);
+                    
+                    
+                %Calculate weighted distance between the two patches        
+
+
+
+                h = 1 - (w1*dist(1:4) + w2*dist(5:20) + w3*dist(21:84))
+                
+                
+
+            end
+            
+            
+            drawnow;
+        else
+            % Simply plot square frame of the patch
+            rectangle('Position', [x-r, y-r, 2*r, 2*r], 'EdgeColor', 'k');
+        end        
+    end
+
+
+    function changePoint(fh,~)
+        drawSquare(fh);
+    end
+
+    function changeRadius(fh,callbackData)
+        r = min(min(H,W)/2, max(1, r + callbackData.VerticalScrollCount));
+        drawSquare(fh);
+    end
+    
+    function changeNumBins(fh)
+        validInput = false;
+        dlgTitle = 'Change number of histogram bins';
+        while ~validInput
+            answer = inputdlg('Enter number of bins:',dlgTitle);
+            if isempty(answer)
+                validInput = true; % keep previous nBins
+            else
+                answer = answer{1};
+                answer = str2double(answer);
+                if isempty(answer) || answer <= 0
+                    dlgTitle = 'Invalid input! #bins must be a positive scalar.';
+                else
+                    numBins = answer;
+                    validInput = true;
                 end
             end
         end
-            
-                   
-            reference_patch = im((y1 : y2), (x1 : x2), :);
-            %figure(1)
-            subplot(3,6,4);
-            imshow(reference_patch);
-            subplot(3,6,10);
-            imshow(reference_patch);
-            hold on;
-            [X,Y]=meshgrid(0:16:33);
-            plot(X,Y,'k');
-            plot(Y,X,'k'); axis off
-            
-            
-            subplot(3,6,16);
-            imshow(reference_patch);
-            hold on;
-            [X,Y]=meshgrid(0:8:33);
-            plot(X,Y,'k');
-            plot(Y,X,'k'); axis off
-            
+        drawSquare(fh);
+    end
 
-            %B = 5;
-            %f1 = patchEncoding(binImage(reference_patch, B), 'hist-normalized', B); 
-            
-        
-end
+    function changeReferencePatch(fh,~)                
+        if strcmp(fh.SelectionType, 'normal')
+            x = round(fh.CurrentAxes.CurrentPoint(1,1));
+            y = round(fh.CurrentAxes.CurrentPoint(1,2));
+            refxy = [x,y];
+            isReferenceFrameSelected = true;
+            selectingNewReference = true;
+        elseif strcmp(fh.SelectionType, 'alt')
+            % Do nothing on right click
+        elseif strcmp(fh.SelectionType, 'extend') 
+            changeNumBins(fh)
+        end
+        drawSquare(fh)
+        selectingNewReference = false;
+    end
 
+    function displaySubpatches(im, k)
+        imshow(im);
+        hold on;
+        [X,Y]=meshgrid(0:k:2*r+2);
+        plot(X,Y,'k');
+        plot(Y,X,'k'); axis off
+    end
 
-
-function changePoint(fh,~)
-        global h1 B ii1 ii2 ii3 ii4 x1ref y1ref imRGB
-        im = imRGB;
-        [H,W,C] = size(im);
-        P = 32;
-        
-        RI = imref2d(size(im));
-        RI.XWorldLimits = [1 W];
-        RI.YWorldLimits = [1 H];
-         
-        subplot(3,6,[1 15])
-        imshow(im, RI, 'InitialMagnification', 'fit');
-        rectangle('Position', [x1ref, y1ref, P, P], 'EdgeColor', 'r');
-        
-        x = round(fh.CurrentAxes.CurrentPoint(1,1));
-        y = round(fh.CurrentAxes.CurrentPoint(1,2));
-        
-        x1 = x - floor(P/2);
-        x2 = x + ceil(P/2) - 1;
-        y1 = y - floor(P/2);
-        y2 = y + ceil(P/2) - 1;
-        
-        P = 32; %patchsize
-        
-        
-        if (x1>= 1 && x2<= W && y1>=1 && y2<=H)
-            
-            r = rectangle('Position', [x1, y1, P, P]);
-            
-            if (x1ref >= 1 && x1ref <= W-P+1 && y1ref >= 1 && y1ref <= H-P+1)
-            
-                h2 = zeros(84, B);
-                %h2 = zeros(4,B);
-                l = 1;
-                for k = 2.^(0:2)
-                    patchSize = P/k;
-                    for i = x1:patchSize:x2-patchSize+1
-                        for j = y1:patchSize:y2-patchSize+1
-
-                            h2(4*l-3, :) = createHistogram(ii1, i, j, patchSize);
-                            h2(4*l-2, :) = createHistogram(ii2, i, j, patchSize);
-                            h2(4*l-1, :) = createHistogram(ii3, i, j, patchSize);
-                            h2(4*l, :) = createHistogram(ii4, i, j, patchSize);
-                            l = l + 1;
-
-                        end
-                    end
-                end
-
-
-
-
-                    second_patch = im((y1 : y2), (x1 : x2), :);
-                    subplot(3,6,6)
-                    imshow(second_patch);
-                    subplot(3,6,12)
-                    imshow(second_patch);
-                    hold on;
-                    [X,Y]=meshgrid(0:16:33);
-                    plot(X,Y,'k');
-                    plot(Y,X,'k'); axis off
-
-
-                    subplot(3,6,18)
-                    imshow(second_patch);
-                    hold on;
-                    [X,Y]=meshgrid(0:8:33);
-                    plot(X,Y,'k');
-                    plot(Y,X,'k'); axis off
-                    %%imshow(reference_patch);
-
-                    %f1 = patchEncoding(binImage(reference_patch, B), 'hist', B) 
-                    %B = 5;
-                    %f2 = patchEncoding(binImage(second_patch, B), 'hist-normalized', B);
-
-
-                    dist = histogramDistance(h1/norm(h1), h2/norm(h2), 'chi2')
-
-                    subplot(3,6,5, 'replace')
-                    [X,Y]=meshgrid(0:1);
-                    hold on;
-                    plot(X,Y,'k');
-                    plot(Y,X,'k'); axis off;
-                    text(0, 0.5, num2str(1 - sum(dist(1:4))/4));
-
-
-                    subplot(3,6,11, 'replace')
-                    [X,Y]=meshgrid(0:2);
-                    hold on;
-                    plot(X,Y,'k');
-                    plot(Y,X,'k'); axis off
-                    l = 5;
-                    for i = 0:1
-                        for j = 1:-1:0
-                            text(i, j+0.5, num2str(1 - sum(dist(l:l+3))/4));
-                            l = l + 4;
-                        end
-                    end
-
-
-                    subplot(3,6,17, 'replace')
-                    [X,Y]=meshgrid(0:4);
-                    hold on;
-                    plot(X,Y,'k');
-                    plot(Y,X,'k'); axis off
-                    l = 21;
-                    for i = 0:3
-                        for j = 3:-1:0
-                            text(i, j+0.5, num2str(1 - sum(dist(l:l+3))/4));
-                            l = l + 4;
-                        end
-                    end
-
-
-                    w1 = 0.125*ones(1,4);
-                    w2 = 0.01875*ones(1,16);
-                    w3 = 0.003125*ones(1,64);
-
-                    %h = 1- sum(dist)/length(dist)
-
-                    h = 1 - w1*dist(1:4) + w2*dist(5:20) + w3*dist(21:84)
+    function displayScores(numCols, l, dist)
+        [X,Y]=meshgrid(0:numCols + 1);
+        hold on;
+        plot(X,Y,'k');
+        plot(Y,X,'k'); axis off;
+        for i = numCols:-1:0
+            for j = 0:numCols
+                text(i, j+0.5, num2str(1 - sum(dist(l:l+C))/(C+1), '%.2f'));
+                l = l + 4;
             end
         end
+    end
+
+    function enc = subpatchesEncode(x,y)
+        l = 1;
+        for k = 2.^(0:2)
+            for i = (-k+1):2:k
+                for j = (-k+1):2:k
+                    Dsub = abs(xx-(x - i*ceil(r/k))) <= ceil(r/k) & abs(yy-(y - j*ceil(r/k))) <= ceil(r/k);
+                    imgSubpatch = imgLab(Dsub,:);
+                    textureSubpatch = imgTexture(Dsub);
+                    imgSubpatch = cat(2, imgSubpatch, textureSubpatch);
+                    enc((C+1)*l-C:(C+1)*l, :) = patchEncoding(binImage(imgSubpatch,numBins),'hist-normalized',numBins);
+                    l = l + 1;
+                end
+            end
+        end
+    end
+
+    function showSimilarityHeatmap(encRefPatch)
+        SimMap = zeros(H, W);
+        for i = r:2*r:W-r
+            for j =r:2*r:H-r
+            patchEncode = subpatchesEncode(i, j);
+            dist = histogramDistance(encRefPatch, patchEncode, 'chi2');
+            SimMap(j-r+1:j+r, i-r+1:i+r) = (1 - (w1*dist(1:4) + w2*dist(5:20) + w3*dist(21:84)))*ones(2*r,2*r);
+            end
+        end
+        figure(2)
+        heatmap(SimMap)
+        title('Similarity to reference patch heat map')
+    end
+
+
+    function showHomogeneityHeatmap()
+        HomMap = zeros(H, W);
+        for i = r:2*r:W-r
+            for j =r:2*r:H-r
+            patchEncode = subpatchesEncode(i, j);
+            h1 = repmat(patchEncode(1:4),1, 16);
+            hom = histogramDistance(h1, patchEncode(21:84), 'chi2');
+            HomMap(j-r+1:j+r, i-r+1:i+r) = (1 - max(hom))*ones(2*r,2*r);
+            end
+        end
+        figure(3)
+        heatmap(HomMap)
+        title('Homogeneity heat map')
+    end
+
 end
